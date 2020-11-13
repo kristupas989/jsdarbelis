54d0c257a78fee5b8227e051817c4389a65c8b32
kristupas989 <71262494+kristupas989@users.noreply.github.com>
2020-11-13 19:42:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const vardas = 'Vardas'
+const pavarde = 'Pavarde'
+
+console.log('Aš esu ${vardas} ${pavarde}');
+
+const brutto = 1200;
+const netto = brutto - brutto * 0.2 - brutto * 0.0698 -brutto * 0.1252
+
+
+console.log('Brutto gaunu ${Brutto}');
+console.log('Netto gaunu ${Netto}');
+console.log('Netto tipas: $(typeof(netto){')
+
+const marius = 'Marius';
+const depšys = 'Depšys';
+const jeromass = 25;
+const petroheiht = 0.58;
+const jeroheight =  1.8;
+const petroMass =  3;
+const mariusBmi = marioMass / Marioheight^3;
+const depšysBmi = depšoMass / (petroheiht = petroheiht);
+const heigher = marioBMi > depšysBMI;
+console.long('ar Mariaus BMI didesnis už Depšio: ${heigher}');
+console.long('Mariaus BMI: ${mariausBMI}');
+console.long('Depšio BMI: ${depšioBMI}');
+if(heigher){
+	console.long('Mariaus BMI didesnis');
+
+} else {
+	console.log('Depšio BMI didesnis');
+}
+
+
